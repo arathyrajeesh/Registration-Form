3119189382570dd7c8cb7a51c940bf7aad51b9b2
Arathy <arathyrajeesh2@gmail.com>
2025-07-27 11:10:17 +0530

    html form declaration commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('myForm');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+
+const usernameError = document.getElementById('usernameError');
+const emailError = document.getElementById('emailError');
+const passwordError = document.getElementById('passwordError');
+const confirmPasswordError = document.getElementById('confirmPasswordError');
+
+const successMsg = document.getElementById('successMsg');
+const preview = document.getElementById('preview');
